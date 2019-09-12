23471a48591d32b3bc54055a59cb240e78617f39
CarnunMP <carnun@hotmail.co.uk>
2019-09-12 12:45:30 +0100

    picture styling refactored

--- src/components/content/Picture.js
+++ src/components/content/Picture.js
@@ -1,12 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledPicture = styled.div`
+    height: 90%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 5%;
+
+    img {
+        height: 100%;
+    }
+`;
 
 function Picture(props) {
     const {pictureURL} = props;
 
     return (
-        <div className="picture">
+        <StyledPicture>
             <img src={pictureURL} alt=""/>
-        </div>
+        </StyledPicture>
     )
 }
 
