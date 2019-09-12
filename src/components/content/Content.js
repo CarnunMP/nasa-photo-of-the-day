40f9bb5259eab751c742cc7417cd2c6b8d133214
CarnunMP <carnun@hotmail.co.uk>
2019-09-12 12:42:50 +0100

    content styling refactored

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Picture from "./Picture";
 import Description from "./Description";
+import styled from "styled-components";
+
+const StyledContent = styled.div`
+    height: 85%; /*No idea why, but 90% creates whitespace at the bottom of the page! */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
 
 function Content(props) {
     const {pictureURL, description} = props;
 
     return (
-        <div className="content">
+        <StyledContent>
             <Picture pictureURL={pictureURL}/>
             <Description description={description}/>
-        </div>
+        </StyledContent>
     )
 }
 
