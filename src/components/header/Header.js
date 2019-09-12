0d3e5bba0a6ba765c57ea25fadb9a44c95386aa6
CarnunMP <carnun@hotmail.co.uk>
2019-09-12 12:35:08 +0100

    header styling refactored

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,27 @@
 import React from "react";
 import Logo from "./Logo";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+    width: 100%;
+    height: 10%;
+    background: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem 0 1rem 0;
+`;
 
 function Header(props) {
     const {title} = props;
 
     return (
-        <header>
+        <StyledHeader>
             <Logo/>
             <div className="title">
                 <h1>{title}</h1>
             </div>
-        </header>
+        </StyledHeader>
     )
 }
 
